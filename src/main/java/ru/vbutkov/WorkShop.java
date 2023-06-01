cd0d2247fe20fe8405f0ffcbe4203be6d8deb46e
vbutkov <bytkov@mail.ru>
2023-06-01 11:12:12 +0300

    add more friends

--- src/main/java/ru/vbutkov/WorkShop.java
+++ src/main/java/ru/vbutkov/WorkShop.java
@@ -10,6 +10,8 @@ public class WorkShop {
                 new Friend("Kate", 19, true, 1),
                 new Friend("Dima", 20, true, 7),
                 new Friend("Olya", 21, false, 0),
+                new Friend("Ira", 22, true, 2),
+                new Friend("Natasha", 23, false, 4),
         };
 
         System.out.println("My friends:" + Arrays.toString(friends));
