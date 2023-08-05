1b0c83b9389900d2ed4645c824d1b83343b150f3
vbutkov <bytkov@mail.ru>
2023-08-05 23:32:47 +0300

    Fisrt workштп commit.

---
+++ src/main/java/ru/vbutkov/list/Homework.java
@@ -0,0 +1,52 @@
+package ru.vbutkov.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Homework {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> listIntegers = new ArrayList<>(Arrays.asList(1, 2, 3, 5, 10));
+        int k = 3;
+
+        List<Double> rollingAverage = getRollingAverage(listIntegers, k);
+        System.out.println(rollingAverage);
+    }
+
+    public static List<Double> getRollingAverage(ArrayList<Integer> listIntegers, int k) {
+        List<List<Integer>> listSubLists = getSubList(listIntegers, k);
+        List<Double> listAverageValues = new ArrayList<>();
+
+        for (List<Integer> list : listSubLists) {
+            double summa = 0.0;
+            for (int i = 0; i < list.size(); i++) {
+                summa += list.get(i);
+            }
+            double average = summa / list.size();
+            listAverageValues.add(average);
+        }
+
+        return listAverageValues;
+    }
+
+    private static List<List<Integer>> getSubList(ArrayList<Integer> listIntegers, int k) {
+        List<List<Integer>> listSubLists = new ArrayList<>();
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < k; i++) {
+            list.add(listIntegers.get(i));
+        }
+        listSubLists.add(new LinkedList<>(list));
+
+        for (int i = k; i < listIntegers.size(); i++) {
+            list.add(listIntegers.get(i));
+            list.remove();
+            listSubLists.add(new LinkedList<>(list));
+        }
+
+        return listSubLists;
+    }
+
+}
